19a3469da0861bd1d84b7e60b653618dd489bf3c
PrajwalGpy <prajwalgpy@gmail.com>
2026-06-30 14:29:32 +0530

    Checkout

--- pages/CheckoutPage.js
+++ pages/CheckoutPage.js
@@ -1,34 +1,52 @@
 export class CheckoutPage {
   constructor(page) {
     this.page = page;
+    this.CheckOutBuuton = page.getByRole("button", { name: "Checkout" });
+    this.UserNameInput = page.getByPlaceholder("First Name");
+    this.LastNameInput = page.getByPlaceholder("Last Name");
+    this.ZipCodeInput = page.getByPlaceholder("Zip/Postal Code");
+    this.ChekoutContinueButton = page.getByRole("button", { name: "Continue" });
+    this.finshCheckOutButton = page.getByRole("button", { name: "Finish" });
     this.itemPrices = page.locator('[data-test="inventory-item-price"]');
     this.subTotal = page.locator('[data-test="subtotal-label"]');
     this.taxTotal = page.locator('[data-test="tax-label"]');
     this.toatalAmount = page.locator('[data-test="total-label"]');
   }
 
+  async autoFillInfo() {
+    await this.CheckOutBuuton.click();
+    await this.UserNameInput.fill("Prajwal");
+    await this.LastNameInput.fill("Poojary");
+    await this.ZipCodeInput.fill("576001");
+    await this.ChekoutContinueButton.click();
+  }
+
+  async chekOut() {
+    await this.finshCheckOutButton.click();
+  }
+
   async getItemPrices() {
-    const priceStrings = await itemPrices.allTextContents();
+    const priceStrings = await this.itemPrices.allTextContents();
     return await priceStrings.map((price) =>
       parseFloat(price.replace("$", "")),
     );
   }
 
-  async getNumericLabelValue(locator){
+  async getNumericLabelValue(locator) {
     const test = await locator.innerText();
-    const match = test.replace(/\d+\.\d/)
-    return match ?  parseFloat(match[0]) : [0]
+    const match = test.replace(/\d+\.\d/);
+    return match ? parseFloat(match[0]) : [0];
   }
 
-  async getSubtotal(){
-    return await this.getNumericLabelValue(this.subTotal)
+  async getSubtotal() {
+    return await this.getNumericLabelValue(this.subTotal);
   }
 
-  async getTax(){
+  async getTax() {
     return await this.getNumericLabelValue(this.taxTotal);
   }
 
-  async getTotal(){
+  async getTotal() {
     return await this.getNumericLabelValue(this.toatalAmount);
   }
 }
---
+++ tests/checkout.spec.js
@@ -0,0 +1,53 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { ProductPage } from "../pages/ProductPage";
+import { CartPage } from "../pages/CartPage";
+import { CheckoutPage } from "../pages/CheckoutPage";
+import { users } from "../test-data/users";
+
+let loginPage, productPage, cartPage, checkoutPage;
+
+test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    productPage = new ProductPage(page);
+    cartPage = new CartPage(page);
+    checkoutPage = new CheckoutPage(page);
+
+    await loginPage.navigate();
+
+    await loginPage.login(
+        users.standardUser.username,
+        users.standardUser.password,
+    );
+
+    const ItemList = [
+        "Sauce Labs Backpack",
+        "Sauce Labs Bolt T-Shirt",
+        "Test.allTheThings() T-Shirt (Red)",
+    ];
+
+    for (let items of ItemList) {
+        await productPage.addItemToCart(items);
+    }
+
+    await productPage.goToCart();
+});
+
+
+test('Checkout Successfully', async({page})=>{
+
+    await checkoutPage.autoFillInfo();
+
+    await expect(page).toHaveURL(/checkout-step-two.html/)
+
+    await checkoutPage.chekOut();
+
+    const CheckoutMessage = page.locator('[data-test="complete-header"]')
+    await expect(CheckoutMessage).toBeVisible();
+})
+
+test('Checkout Empty Cart',async({page})=>{
+
+})
+
+
